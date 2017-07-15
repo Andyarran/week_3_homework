8d7e7f3a604f4d3b34062e685c5ae56f8f07e03a
Andrew Weir <aweir@europe.com>
2017-07-15 11:22:03 +0100

    Figuring out screening part, suppose i need a qty etc

--- models/screening.rb
+++ models/screening.rb
@@ -12,7 +12,31 @@ def initialize(options)
   @showing = options['showing']
 end
 
+def save()
+  sql = "INSERT INTO screenings(screening_id, showing) VALUES (#{ @screening_id }, #{@showing}) RETURNING id;"
+  user = SqlRunner.run(sql)[0]
+  @id = user['id'].to_i
+end
 
+def update()
+  sql = "UPDATE screenings SET ( screening_id, showing ) = ( #{@screening_id}, #{@showing} );"
+  SqlRunner.run(sql)
+end
+
+
+def delete_one
+  sql = "DELETE FROM screenings WHERE id = #{@id}"
+  SqlRunner.run(sql)
+end
+
+
+def most_popular()
+  sql "SELECT FROM films
+  INNER JOIN tickets
+  ON customers.id = tickets.customer_id
+  WHERE customer_id = #{@id};"
+  SqlRunner.run(sql).count
+end
 
 
 
